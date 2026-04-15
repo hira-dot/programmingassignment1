be860d58595227e7d550cf318806ccd0f0a32605
hira-dot <hirasshakeel06@gmail.com>
2026-04-15 12:03:31 -0400

    fix variable name / formatting issues

--- program 3.js
+++ program 3.js
@@ -4,14 +4,14 @@ const readlineSync = require('readline');
 let str = readlineSync.question("Enter a string: ").tolowercase();
 let freq = {}; let firstRepeat = ""; // storage
 for (let i = 0; i < str.length; i++) {
-    let char = str[i] // loop thru string
-    if (char >= 'a' && char <= 'z') // include letters a-z
-    if (freq[char] === undefined) {
-        freq[char] = 1;
+    let cha = str[i] // loop thru string
+    if (cha >= 'a' && cha <= 'z') // include letters a-z
+    if (freq[cha] === undefined) {
+        freq[cha] = 1;
     } else {
-        freq[char]++; // counting letters
-        if(freq[char] === 2 && firstRepeat === ""){
-            firstRepeat = char; // identify the 1st repeat
+        freq[cha]++; // counting letters
+        if(freq[cha] === 2 && firstRepeat === ""){
+            firstRepeat = cha; // identify the 1st repeat
 console.log("Letter Frequencies:");
 for(let key in freq) {
     console.log(key + ": " + freq[key]); // frequency table 
