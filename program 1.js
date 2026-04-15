bd4bdee7466efcab7ae88756125005e9d0137f3b
hira-dot <hirasshakeel06@gmail.com>
2026-04-15 12:14:39 -0400

    Update program 1.js

--- program 1.js
+++ program 1.js
@@ -1,7 +1,7 @@
 // program 1: detect prime gaps
-// Hira Shakeel 
+// hira shakeel 
 const readlineSync = require('readline-sync');
-let n = Number(readlineSync.question("enter a number (<=1000)"));
+let n = Number(readlineSync.question("enter a number (<=1000): "));
 function isPrime(num) {
     if (num < 2) return false;
     for (let i = 2; i < num; i++){
@@ -10,34 +10,31 @@ function isPrime(num) {
         }
     }
     return true; // verify prime
-    let primes = [];
-    for (let i = 2; i <= n; i++) {
-        if (isPrime(i)){
-            primes.push(i); //collect primes
+}
+let primes = [];
+for (let i = 2; i <= n; i++) {
+    if (isPrime(i)){
+        primes.push(i); //collect primes
+    }
+}
+let gaps = [];
+for (let i = 1; i < primes.length; i++){
+    gaps.push(primes[i] - primes[i - 1]); //identify gaps
+}
+let maxGap = gaps[0];
+let index = 0;
+for (let i = 0; i < gaps.length; i++){
+    if(gaps[i] > maxGap) {
+        maxGap = gaps[i];
+        index = i; // biggest gap
+    }
+}
+let sum = 0;
 
-            let gaps = [];
-            for (let i = i; i < primes.length; i++){
-                gaps.push(primes[i] - primes[i-1]); //identify gaps
-
-    let maxGap = gaps[0];
-    let index = 0;
-    for (let i = i; i < gaps.length; i++){
-        if(gaps[i] > maxGap) {
-            maxGap = gaps[i];
-            index = i; // biggest gap
-
-        let sum = 0;
-        for(let i = 0; i < gaps.length; i++){
-            sum += gaps[i];
-            let avg = sum / gaps.length; //avg gap
+for(let i = 0; i < gaps.length; i++){
+    sum += gaps[i];
+}
+let avg = sum / gaps.length; //avg gap
 console.log("primes:", primes);
 console.log("the largest gap is " + maxGap + ", between " + primes[index] + " and " + primes[index + 1]);
-console.log("the average gap is " + avg.toFixed(2));
-        }
-        }
-    }
-            }
-        }
-    }
-
-}
+console.log("the average gap is " + avg.toFixed(2)); //output
